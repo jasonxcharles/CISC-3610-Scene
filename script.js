9fc9926aa1271a437d635c07483f068f7cd36d80
Jason A. Charles <93563310+jasonxcharles@users.noreply.github.com>
2025-05-18 00:34:53 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const background = new Image();
+background.src = "images/background.jpg";
+background.onload = function () {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  const char1 = new Image();
+  char1.src = "images/character1.png";
+  char1.onload = function () {
+    ctx.drawImage(char1, 100, 350, 180, 180);
+
+    const char2 = new Image();
+    char2.src = "images/character2.png";
+    char2.onload = function () {
+      ctx.drawImage(char2, 520, 340, 180, 180);
+
+      ctx.font = "28px Arial";
+      ctx.fillStyle = "white";
+      ctx.fillText("Jason A. Charles", 30, 40);
+      ctx.fillText("Academic Warriors", 30, 80);
+    };
+  };
+};
