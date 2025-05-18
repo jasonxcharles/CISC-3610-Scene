7da00a1c0652e26f6f4a1521aeffde0dbe15b85c
Jason A. Charles <93563310+jasonxcharles@users.noreply.github.com>
2025-05-18 01:22:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,33 @@ const ctx = canvas.getContext("2d");
 
 const background = new Image();
 background.src = "images/background.jpg";
-background.onload = function () {
+
+const char1 = new Image();
+char1.src = "images/character1.jpg";
+
+const char2 = new Image();
+char2.src = "images/character2.jpg";
+
+let loadedImages = 0;
+
+function drawScene() {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(char1, 100, 350, 180, 180);
+  ctx.drawImage(char2, 520, 340, 180, 180);
 
-  const char1 = new Image();
-  char1.src = "images/character1.jpg";
-  char1.onload = function () {
-    ctx.drawImage(char1, 100, 350, 180, 180);
+  ctx.font = "28px Arial";
+  ctx.fillStyle = "white";
+  ctx.fillText("Jason A. Charles", 30, 40);
+  ctx.fillText("Academic Warriors", 30, 80);
+}
 
-    const char2 = new Image();
-    char2.src = "images/character2.jpg";
-    char2.onload = function () {
-      ctx.drawImage(char2, 520, 340, 180, 180);
+function imageLoaded() {
+  loadedImages++;
+  if (loadedImages === 3) {
+    drawScene();
+  }
+}
 
-      ctx.font = "28px Arial";
-      ctx.fillStyle = "white";
-      ctx.fillText("Jason A. Charles", 30, 40);
-      ctx.fillText("Academic Warriors", 30, 80);
-    };
-  };
-};
+background.onload = imageLoaded;
+char1.onload = imageLoaded;
+char2.onload = imageLoaded;
