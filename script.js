04a806ac6e2db5ec25535904c94ed3d8b591ffac
Jason A. Charles <93563310+jasonxcharles@users.noreply.github.com>
2025-05-18 01:15:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ background.onload = function () {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
   const char1 = new Image();
-  char1.src = "images/character1.png";
+  char1.src = "images/character1.jpg";
   char1.onload = function () {
     ctx.drawImage(char1, 100, 350, 180, 180);
 
     const char2 = new Image();
-    char2.src = "images/character2.png";
+    char2.src = "images/character2.jpg";
     char2.onload = function () {
       ctx.drawImage(char2, 520, 340, 180, 180);
 
